020d0cae0645e91f2baa92ffcbe33d81ed2e32dc
mbmugdho <mdmontasirbillah5@gmail.com>
2025-11-12 22:57:19 +0600

    added firebase SDK

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzCS0jw-oWe4lNX3RhBiBlAtxAiaxU-68",
+  authDomain: "homehero-2e454.firebaseapp.com",
+  projectId: "homehero-2e454",
+  storageBucket: "homehero-2e454.firebasestorage.app",
+  messagingSenderId: "951095827613",
+  appId: "1:951095827613:web:ea6dfbe2f7ff5566684874"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
